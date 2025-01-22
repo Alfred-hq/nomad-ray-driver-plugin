008c433cbbccb9582b7ea0a19b5c02ff9199d086
jpatidar30 <jpatidar@deqode.com>
2025-01-22 15:30:33 +0530

    inital setup

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ package main
 
 import (
 	// TODO: update the path below to match your own repository
-	"github.com/hashicorp/nomad-skeleton-driver-plugin/hello"
+	"github.com/Alfred-hq/nomad-ray-driver-plugin/ray"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/nomad/plugins"
